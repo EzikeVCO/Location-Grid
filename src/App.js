d50b61137785a13ab312f97136c699e7925bb8cb
ezikechris <christophermbe1009@gmail.com>
2021-03-02 13:49:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Stats from './Stats/Stats';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Stats/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Stats/Stats.js
@@ -0,0 +1,16 @@
+import React, { Component} from  'react';
+
+
+
+class Stats extends Component {
+    render() {
+        return(
+        <div>
+            <h1>HELLOS</h1>
+        </div>
+         );
+    }
+}
+
+
+export default Stats;
