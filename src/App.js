3707270b93689309a383d80ee9a29e21aa411c30
ezikechris <christophermbe1009@gmail.com>
2021-03-02 22:07:41 +0100

    Add Location Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import Stats from './Stats/Stats';
+import Location from './Location/Location'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Stats/>
+      <Location/>
+
     </div>
   );
 }
